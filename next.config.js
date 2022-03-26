e86d25ae3251e2c505c80f94cb1bca8965455dd2
Denys Lyepustin <lyepustin@gmail.com>
2022-03-26 23:16:38 +0100

    added lenguages metainfo to i18n

--- next.config.js
+++ next.config.js
@@ -2,7 +2,7 @@ const { i18n } = require("./next-i18next.config");
 
 module.exports = {
   i18n: {
-    locales: ["en", "es"],
+    locales: ["en", "es", "ru", "ua"],
     defaultLocale: "es",
     localeDetection: false
   }
