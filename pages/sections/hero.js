1ae0d395c157b1ef07389f9699d687b46fd9b974
Denys Lyepustin <lyepustin@gmail.com>
2022-04-10 23:08:14 +0200

    images optimization

--- pages/sections/hero.js
+++ pages/sections/hero.js
@@ -1,30 +1,30 @@
 import { useTranslation } from "next-i18next";
+import Image from "next/image";
+import heroPic from "/public/images/hero.jpg";
 
 const Hero = () => {
   const { t } = useTranslation();
   return (
     <div id="hero-area" className="hero-slider">
-      {/* <!--Hero Slide Item Start--> */}
       <div className="hs-item" style={{ backgroundImage: "url(images/heroBackgroud.jpg)" }}>
         <div className="container">
           <div className="row">
-            {/* <!--Hero Slide Content--> */}
             <div className="hs-item-wrapper col-12 d-flex">
               <div className="row">
                 <div className="col-xl-8 col-lg-6 col-md-8 col-9 offset-lg-1 offset-xxl-0">
                   <div className="hs-content">
-                    {/* <h2>{t("human effort")}</h2> */}
                     <h1>{t("how we are")}</h1>
                     <p>{t("our mission")}</p>
-                    {/* <a href="#about-area" data-scroll className="btn btn-xlg">
-                      {t("join with us")}{" "}
-                      <i className="zmdi zmdi-long-arrow-right"></i>
-                    </a> */}
                   </div>
                 </div>
               </div>
               <div className="hs-image">
-                <img className="img-fluid" src="images/hero.jpg" alt="" />
+                <Image
+                  src={heroPic}
+                  alt="Picture of the author"
+                  placeholder="blur" // Optional blur-up while loading
+                />
+                {/* <img className="img-fluid" src="images/hero.jpg" alt="" /> */}
               </div>
             </div>
           </div>
