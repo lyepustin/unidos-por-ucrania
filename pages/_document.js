9ceaa7cb83ba26f810a285138eebfa3f00ffa5cd
Denys Lyepustin <lyepustin@gmail.com>
2022-04-12 22:08:06 +0200

    Open Graph Meta Data

--- pages/_document.js
+++ pages/_document.js
@@ -8,12 +8,17 @@ export default function Document() {
         <link rel="shortcut icon" type="image/x-icon" href="/images/favicon.ico" />
 
         {/* Customize Social Media Preview of your Website Links */}
+        <meta property="og:site_name" content="Unidos por Ucrania - Ayuda humanitaria" />
+        <meta property="og:title" content="Unidos por Ucrania" />
+        <meta property="og:description" content="Grupo de iniciativas de ayuda humanitaria a Ucrania desde España" />
+
         <meta property="og:url" content="https://unidosporucrania.ml/" />
         <meta property="og:type" content="website" />
-        <meta property="og:title" content="Unidos por Ucrania" />
-        <meta name="twitter:card" content="summary" />
-        <meta property="og:description" content="Grupo de iniciativas de ayuda humanitaria a Ucrania desde España" />
-        <meta property="og:image" content="/images/logo.png" />
+        <meta property="og:locale" content="es_es" />
+
+        <meta property="og:image" content="https://unidosporucrania.ml/images/logo.png" />
+        {/* <meta property="og:image:width" content="200" />
+        <meta property="og:image:height" content="200" /> */}
       </Head>
       <body>
         <Main />
